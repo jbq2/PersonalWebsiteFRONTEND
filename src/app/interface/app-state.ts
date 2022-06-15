8afce0842f78eda2cd3fd1cb8869548e4f843ce9
Joshua Quizon <joshquizon@gmail.com>
2022-06-14 21:44:10 -0400

    made an appstate interface to track the state of the app when it's running

--- src/app/interface/app-state.ts
+++ src/app/interface/app-state.ts
@@ -1,3 +1,6 @@
-export interface AppState{
-    
+import { DataState } from './../enum/data-state.enum';
+export interface AppState<T> {
+    dataState: DataState;
+    appData?: T;
+    error?: string; 
 }
