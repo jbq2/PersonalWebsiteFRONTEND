ed1cc89a01aa8db495c68abc1e774f8cbfd62b86
Joshua Quizon <joshquizon@gmail.com>
2022-06-15 13:06:44 -0400

    mapped backend http requests to frontend http requests

--- src/app/service/project.service.ts
+++ src/app/service/project.service.ts
@@ -28,6 +28,23 @@ export class ProjectService {
     catchError(this.handleError)
   );
 
+
+  project$ = (id: number) => <Observable<CustomResponse>>
+  this.http.get<CustomResponse>(`${this.url}/get/${id}`)
+  .pipe(
+    tap(console.log),
+    catchError(this.handleError)
+  );
+
+
+  projectByName$ = (name: string) => <Observable<CustomResponse>>
+  this.http.get<CustomResponse>(`${this.url}/getByName/${name}`)
+  .pipe(
+    tap(console.log),
+    catchError(this.handleError)
+  );
+
+
   save$ = (project: Project) => <Observable<CustomResponse>> 
   this.http.post<CustomResponse>(`${this.url}/save`, project)
   .pipe(
@@ -35,12 +52,30 @@ export class ProjectService {
     catchError(this.handleError)
   );
 
+
   update$ = (project: Project) => <Observable<CustomResponse>> 
   this.http.post<CustomResponse>(`${this.url}/update`, project)
   .pipe(
     tap(console.log),
     catchError(this.handleError)
   );
+
+
+  delete$ = (id: number) => <Observable<CustomResponse>>
+  this.http.delete<CustomResponse>(`${this.url}/delete/${id}`)
+  .pipe(
+    tap(console.log),
+    catchError(this.handleError)
+  );
+
+
+  deleteByName$ = (name: string) => <Observable<CustomResponse>>
+  this.http.delete<CustomResponse>(`${this.url}/deleteByName/${name}`)
+  .pipe(
+    tap(console.log),
+    catchError(this.handleError)
+  );
+
 
   handleError(handleError: any): Observable<never> {
     return throwError ('Method not implemented.');
