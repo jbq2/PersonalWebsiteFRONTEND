4e85e6a95e3d5ec2b7328d61ef970a12ac57b758
Joshua Quizon <joshquizon@gmail.com>
2022-06-16 13:54:23 -0400

    moving projects stuff to project component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { ProjectsComponent } from './component/projects/projects.component';
+import { HomeComponent } from './component/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'projects', component: ProjectsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/component/projects/projects.component.ts
+++ src/app/component/projects/projects.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { catchError, map, Observable, of, startWith } from 'rxjs';
+import { DataState } from 'src/app/enum/data-state.enum';
+import { AppState } from 'src/app/interface/app-state';
+import { CustomResponse } from 'src/app/interface/custom-response';
+import { ProjectService } from 'src/app/service/project.service';
 
 @Component({
   selector: 'app-projects',
@@ -7,9 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+  appState$: Observable<AppState<CustomResponse>> | undefined;
+  constructor(private projectService: ProjectService) {}
 
   ngOnInit(): void {
+    this.appState$ = this.projectService.projects$
+    .pipe(
+      map(response => {
+        return { dataState: DataState.LOADED_STATE, appData: response }
+      }),
+      startWith({ dataState: DataState.LOADING_STATE }),
+      catchError((error: string) => {
+        return of({ dataState: DataState.ERROR_STATE, error })
+      })
+    );
   }
 
 }
