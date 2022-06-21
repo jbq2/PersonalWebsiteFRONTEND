d47e0970025d92240fbfc837e6cc2a0594030af5
Joshua Quizon <joshquizon@gmail.com>
2022-06-21 18:07:54 -0400

    added github contributions to home page, minor changes to about me

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -5,10 +5,31 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
+  date: Date;
+  dateStr: string;
+  monthMap: Map<number, string>;
+  constructor() { 
 
-  ngOnInit(): void { }
+    this.date = new Date();
+
+    this.monthMap = new Map<number, string>([
+      [0, 'January'],
+      [1, 'February'],
+      [2, 'March'],
+      [3, 'April'],
+      [4, 'May'],
+      [5, 'June'],
+      [6, 'July'],
+      [7, 'August'],
+      [8, 'September'],
+      [9, 'October'],
+      [10,'November'],
+      [11,'December']
+    ])
+
+    this.dateStr = `${this.date.getDate()} ${this.monthMap.get(this.date.getMonth())}, ${this.date.getFullYear()}`
+  }
 
 }
