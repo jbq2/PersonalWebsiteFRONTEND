4fe0b187784eb1ccdaf813e9bca24c93f89517d9
Joshua Quizon <joshquizon@gmail.com>
2022-06-22 22:40:34 -0400

    did basic info and projects list, made a skeleton for other subsections

--- src/app/component/resume/resume.component.ts
+++ src/app/component/resume/resume.component.ts
@@ -1,4 +1,7 @@
+import { ProjectService } from './../../service/project.service';
+import { Project } from './../../interface/project';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-resume',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResumeComponent implements OnInit {
 
-  constructor() { }
+  projects!: Project[];
+  constructor(private projectService: ProjectService, private router: Router) {
+
+    this.projects = [];
+   }
 
   ngOnInit(): void {
+    this.projectService.getProjects().subscribe(response => {
+      this.projects = response.data["projects"];
+
+      let chars = [];
+      console.log(this.projects.length);
+      for(let i = 0; i < this.projects.length; i++){
+        let shortDesc = '';
+        chars = [...this.projects[i].description];
+  
+        for(let j = 0; j < chars.length && chars[j] != '.'; j++){
+          shortDesc += chars[j];
+        }
+  
+        console.log(shortDesc);
+        this.projects[i].description = shortDesc;
+      }
+    })
+  }
+
+  onClickProject(id: number){
+    this.router.navigate(['/project'], {
+      queryParams: { 'id': id }
+    });
   }
 
 }
